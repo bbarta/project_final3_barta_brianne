0df3f7fa44f8d2e904dd795b498f73e7d1f52299
Brianne Barta <brianne28@ufl.edu>
2017-04-22 13:47:11 -0400

    add social icons and button in footer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-onclick="document.getElementById('demo').style.display='block'"
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("topbtn").style.display = "block";
+    } else {
+        document.getElementById("topbtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
